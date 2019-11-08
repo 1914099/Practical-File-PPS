755e2f735773abc977fe7a174b07ead016840efe
Sarabjeet Kaur <56109650+1914099@users.noreply.github.com>
2019-11-08 12:46:15 +0530

    Create simple_interest.c

---
+++ simple_interest.c
@@ -0,0 +1,12 @@
+/* Calculation of simple interest */ 
+main( ) 
+{ 
+ int p, n ; 
+ float r, si ; 
+ p = 1000 ; 
+ n = 4 ; 
+ r = 7.3 ; 
+ /* formula for simple interest */ 
+ si = p * n * r / 100 ; 
+ printf ( "%f" , si ) ; 
+}
