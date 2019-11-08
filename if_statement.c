76085ad723da798f9956dd16bc02f29a8ca9f737
Sarabjeet Kaur <56109650+1914099@users.noreply.github.com>
2019-11-08 12:41:14 +0530

    Create if_statement.c

---
+++ if_statement.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/* Demonstration of if statement */ 
+int main( ) 
+{ 
+ int num ; 
+ printf ( "Enter a number less than 7 " ) ; 
+ scanf ( "%d", &num ) ; 
+ if ( num <= 7 ) 
+ printf ( "Great job !" ) ; 
+} 
